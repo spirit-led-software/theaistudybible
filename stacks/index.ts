d71c1668ef59e0723208eff36e5216e671caf4e5
Ian Pascoe <ianpascoe@Ians-MBP.ssid1>
2023-07-01 13:04:12 -0400

    Refactoring into monorepo without SST

--- stacks/index.ts
+++
@@ -1,5 +0,0 @@
-export { default as BackendStack } from "./backend";
-export { default as CacheStack } from "./cache";
-export { default as DatabaseStack } from "./database";
-export { default as S3Stack } from "./s3";
-export { default as SharedStack } from "./shared";
