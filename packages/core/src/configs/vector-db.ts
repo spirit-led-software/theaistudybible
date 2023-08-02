2dfd592593974c84674c5a4575170c025cde2b34
Ian Pascoe <ianpascoe@Ians-MacBook-Pro.local>
2023-08-02 10:55:22 -0400

    PG vector migration

--- packages/core/src/configs/vector-db.ts
+++ packages/core/src/configs/vector-db.ts
@@ -1,17 +1,11 @@
 interface VectorDBConfig {
   url: string;
-  apiKey: string;
   collectionName: string;
-  collectionDimensions: number;
 }
 
 export const config: VectorDBConfig = {
   url: process.env.VECTOR_DB_URL as string,
-  apiKey: process.env.VECTOR_DB_API_KEY as string,
   collectionName: process.env.VECTOR_DB_COLLECTION_NAME as string,
-  collectionDimensions: parseInt(
-    process.env.VECTOR_DB_COLLECTION_DIMENSIONS as string
-  ),
 };
 
 export default config;
