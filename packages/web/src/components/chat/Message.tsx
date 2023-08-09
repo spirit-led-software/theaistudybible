8d7be0508c35cb5cd6628f8f2bd1f560eacd449d
Ian Pascoe <ianpascoe@Ians-MacBook-Pro.local>
2023-08-09 19:49:02 -0400

    Advertisement functionality

--- packages/web/src/components/chat/Message.tsx
+++ packages/web/src/components/chat/Message.tsx
@@ -2,24 +2,25 @@
 
 import { Avatar } from "@components/user";
 import { dimensionClasses } from "@lib/sizing";
+import { Message as ChatMessage } from "ai/react";
 import { TbCross } from "react-icons/tb";
 import { ResponseSources } from "./ResponseSources";
 
 export function Message({
   chatId,
-  id,
-  text,
-  sender,
+  message,
+  prevMessage,
 }: {
   chatId: string;
-  id: string;
-  text: string;
-  sender: string;
+  message: ChatMessage;
+  prevMessage?: ChatMessage;
 }) {
+  const { id, content, role } = message;
+
   return (
     <div className="flex flex-row items-center w-full px-2 py-4 overflow-x-hidden bg-white border border-t-slate-300">
       <div className="flex flex-col content-start">
-        {sender === "user" ? (
+        {role === "user" ? (
           <Avatar size="md" className="border shadow-lg" />
         ) : (
           <TbCross
@@ -28,8 +29,8 @@ export function Message({
         )}
       </div>
       <div className="flex flex-col w-full pl-5 pr-3 overflow-x-clip">
-        <div className="w-full break-words whitespace-pre-wrap">{text}</div>
-        {sender !== "user" && (
+        <div className="w-full break-words whitespace-pre-wrap">{content}</div>
+        {role !== "user" && (
           <ResponseSources aiResponseId={id} chatId={chatId} />
         )}
       </div>
--- packages/web/src/components/chat/Window.tsx
+++ packages/web/src/components/chat/Window.tsx
@@ -258,16 +258,19 @@ export function Window({
         {messages && messages.length > 0 ? (
           <div className="w-full h-full overflow-y-scroll">
             <div className="flex flex-col flex-1 min-h-full place-content-end">
-              {messages.map((message) => (
-                <Message
-                  key={message.id}
-                  chatId={initChatId!}
-                  id={message.id}
-                  text={message.content}
-                  sender={message.role}
-                />
+              {messages.map((message, index) => (
+                <div key={message.id} className="flex flex-col w-full">
+                  {/* Randomly show an ad */}
+                  {index % Math.floor(Math.random() * 10) === 0 && (
+                    <AdMessage />
+                  )}
+                  <Message
+                    chatId={initChatId!}
+                    message={message}
+                    prevMessage={messages[index - 1]}
+                  />
+                </div>
               ))}
-              {isLoading && <LoadingMessage />}
               <div ref={endOfMessagesRef} className="w-full h-16" />
             </div>
           </div>
