92a2d5ba55b97d1ca26167625e57a3ea7efdf6f8
Ian Pascoe <ianpascoe@Ians-MacBook-Pro.local>
2023-08-10 12:31:28 -0400

    Adding comment

--- packages/core/src/services/vector-db.ts
+++ packages/core/src/services/vector-db.ts
@@ -21,7 +21,7 @@ export async function getVectorStore() {
             },
           ],
         },
-        ssl: true,
+        ssl: true, // always use SSL when connecting to NeonDB
       },
     }
   );
