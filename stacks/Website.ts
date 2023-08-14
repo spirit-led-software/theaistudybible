f1e6c3f2482273ceb8e60ae1dcd450d31b8df483
Ian Pascoe <ianpascoe@Ians-MacBook-Pro.local>
2023-08-14 17:27:45 -0400

    Final tweaks

--- stacks/Website.ts
+++ stacks/Website.ts
@@ -7,12 +7,6 @@ export function Website({ stack }: StackContext) {
   const { indexFileBucket, devotionImageBucket } = use(S3);
   const { api, apiUrl, chatApiUrl } = use(API);
   const { hostedZone, domainName, websiteUrl } = use(Constants);
-  const {
-    dbReadOnlyUrl,
-    dbReadWriteUrl,
-    vectorDbReadOnlyUrl,
-    vectorDbReadWriteUrl,
-  } = use(DatabaseScripts);
 
   const website = new NextjsSite(stack, "website", {
     path: "packages/web",
@@ -24,10 +18,6 @@ export function Website({ stack }: StackContext) {
       NEXT_PUBLIC_CHAT_API_URL: chatApiUrl,
       INDEX_FILE_BUCKET: indexFileBucket.bucketName,
       DEVOTION_IMAGE_BUCKET: devotionImageBucket.bucketName,
-      DATABASE_READWRITE_URL: dbReadWriteUrl,
-      DATABASE_READONLY_URL: dbReadOnlyUrl,
-      VECTOR_DB_READWRITE_URL: vectorDbReadWriteUrl,
-      VECTOR_DB_READONLY_URL: vectorDbReadOnlyUrl,
       ...STATIC_ENV_VARS,
     },
     customDomain: {
