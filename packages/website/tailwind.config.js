eb19d64471038a58b8125b3588cfa478283d2e56
Ian Pascoe <ianpascoe@Ians-MacBook-Pro.local>
2023-08-24 05:47:24 -0400

    Hopefully fixing flowbite things

--- packages/website/tailwind.config.js
+++ packages/website/tailwind.config.js
@@ -1,12 +1,51 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  darkMode: 'class',
-  content: ["./src/**/*.{html,js,svelte,ts}"],
-  theme: {
-    fontFamily: {
-      catamaran: ["Catamaran"],
-    },
-    extend: {},
-  },
-  plugins: [require('tailwindcss-animate'), require("tailwindcss-brand-colors"), require('flowbite/plugin')],
+	content: [
+		'./src/**/*.{html,js,svelte,ts}',
+		'./node_modules/flowbite-svelte/**/*.{html,js,svelte,ts}'
+	],
+	plugins: [
+		require('flowbite/plugin'),
+		require('tailwindcss-animate'),
+		require('tailwindcss-brand-colors')
+	],
+	darkMode: 'class',
+	theme: {
+		fontFamily: {
+			catamaran: ['Catamaran']
+		},
+		extend: {
+			colors: {
+				// SlateGray
+				primary: {
+					50: '#f8fafc',
+					100: '#f1f5f9',
+					200: '#e2e8f0',
+					300: '#cbd5e1',
+					400: '#94a3b8',
+					500: '#64748b',
+					600: '#475569',
+					700: '#334155',
+					800: '#1e293b',
+					900: '#0f172a'
+				},
+				// SkyBlue
+				secondary: {
+					50: '#f0f9ff',
+					100: '#e0f2fe',
+					200: '#bae6fd',
+					300: '#7dd3fc',
+					400: '#38bdf8',
+					500: '#0ea5e9',
+					600: '#0284c7',
+					700: '#0369a1',
+					800: '#075985',
+					900: '#0c4a6e'
+				}
+			}
+		}
+	},
+	future: {
+		hoverOnlyWhenSupported: true
+	}
 };
