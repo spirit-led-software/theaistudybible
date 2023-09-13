8654a9c42f441ac5bd13d6532dae7aba6d765aa7
Ian Pascoe <ianpascoe@Ians-Mac.local>
2023-09-13 17:44:31 -0400

    Removing stripe code

--- mobile/lib/main.dart
+++ mobile/lib/main.dart
@@ -3,12 +3,10 @@ import 'dart:async';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_stripe/flutter_stripe.dart';
 import 'package:flutter_timezone/flutter_timezone.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:revelationsai/firebase_options.dart';
 import 'package:revelationsai/src/app.dart';
-import 'package:revelationsai/src/constants/stripe.dart';
 import 'package:revelationsai/src/utils/state_logger.dart';
 import 'package:timezone/data/latest_all.dart' as tz;
 import 'package:timezone/timezone.dart' as tz;
@@ -24,9 +22,6 @@ Future<void> main() async {
     options: DefaultFirebaseOptions.currentPlatform,
   );
   messaging.subscribeToTopic('daily-devo');
-
-  Stripe.publishableKey = StripeConfig.publicKey;
-  await Stripe.instance.applySettings();
 
   runApp(
     const ProviderScope(
--- mobile/lib/src/constants/stripe.dart
+++
@@ -1,3 +0,0 @@
-class StripeConfig {
-  static const String publicKey = String.fromEnvironment('STRIPE_PUBLIC_KEY');
-}
