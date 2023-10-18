4f5d089b0e778087e9188558ed7d72cd59caddf8
PassGo Co <122142608+passgoco@users.noreply.github.com>
2023-10-18 07:31:12 -0400

    Update database.ts

--- packages/functions/src/lib/database.ts
+++ packages/functions/src/lib/database.ts
@@ -3,15 +3,10 @@ import * as schema from "@core/database/schema";
 import { neon, neonConfig } from "@neondatabase/serverless";
 import { drizzle } from "drizzle-orm/neon-http";
 
-export const readOnlyDatabase = drizzle(
-  neon(databaseConfig.readOnlyUrl, {
-    readOnly: true,
-  }),
-  {
-    schema,
-    logger: envConfig.isLocal,
-  }
-);
+export const readOnlyDatabase = drizzle(neon(databaseConfig.readOnlyUrl), {
+  schema,
+  logger: envConfig.isLocal,
+});
 
 export const readWriteDatabase = drizzle(neon(databaseConfig.readWriteUrl), {
   schema,
