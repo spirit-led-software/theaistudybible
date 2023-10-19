7a9fef680728eef5c2bbf529680ac08804b1973d
Ian Pascoe <ianpascoe@Ians-Mac-3.local>
2023-10-19 08:40:10 -0400

    A couple more tweaks to the router chain

--- packages/core/src/langchain/chains/router/rai-chat-multi-route.ts
+++ packages/core/src/langchain/chains/router/rai-chat-multi-route.ts
@@ -12,11 +12,11 @@ import { z } from "zod";
 
 const ROUTER_TEMPLATE = (
   formatting: string
-) => `Given a query to a question answering system, select the system best suited for the input. You will be given the names of the available systems and a description of what questions the system is best suited for. You may also revise the original input if you think that revising it will ultimately lead to a better response. The formatting must match the the following instructions exactly.
+) => `Given a query to a question answering system, select the system best suited for the input. You will be given the names of the available systems and a description of what questions the system is best suited for. The formatting must match the the following instructions exactly. If you do not know which one would be best, just return "DEFAULT" without the quotes.
 
 The formatting instructions are within <format_instructions></format_instructions> XML tags.
 The candidate systems are within <candidates></candidates> XML tags. IMPORTANT: The candidates are in the format of "[name]: [description]" where [name] is the name of the question answering system and [description] is a description of what questions the system is best suited for. Only the name of the system should be returned.
-The input is within <input></input> XML tags.
+The input is within <input></input> XML tags. IMPORTANT: Do not modify the input in any way.
 
 <format_instructions>
 ${formatting}
@@ -66,14 +66,16 @@ export class RAIChatMultiRouteChain extends MultiRouteChain {
       destination: z
         .string()
         .optional()
-        .describe('name of the question answering system to use or "DEFAULT"'),
+        .describe(
+          'The name of the question answering system to use or "DEFAULT".'
+        ),
       next_inputs: z
         .object({
           query: z
             .string()
-            .describe("a potentially modified version of the original input"),
+            .describe("The original input as it was given exactly."),
         })
-        .describe("input to be fed to the next model"),
+        .describe("The input to be fed into the next model."),
     });
 
     const outputParser = new RouterOutputParser<
