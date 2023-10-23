670fd261ef89d2362e2d4f8c5e39132572af7930
Ian Pascoe <ianpascoe@Ians-Mac-3.local>
2023-10-23 17:05:49 -0400

    Changing some stacks in non-prod mode

--- stacks/Crons.ts
+++ stacks/Crons.ts
@@ -1,7 +1,7 @@
 import { Constants, DatabaseScripts, S3, STATIC_ENV_VARS } from "@stacks";
 import { Cron, StackContext, dependsOn, use } from "sst/constructs";
 
-export function Crons({ stack }: StackContext) {
+export function Crons({ stack, app }: StackContext) {
   dependsOn(DatabaseScripts);
 
   const { invokeBedrockPolicy } = use(Constants);
@@ -13,34 +13,38 @@ export function Crons({ stack }: StackContext) {
     vectorDbReadOnlyUrl,
   } = use(DatabaseScripts);
 
-  const dailyDevotionCron = new Cron(stack, "dailyDevoCron", {
-    schedule: "cron(0 12 * * ? *)",
-    job: {
-      function: {
-        handler: "packages/functions/src/daily-devo.handler",
-        bind: [devotionImageBucket],
-        permissions: [devotionImageBucket, invokeBedrockPolicy],
-        copyFiles: [
-          {
-            from: "firebase-service-account.json",
-            to: "firebase-service-account.json",
+  if (app.stage === "prod") {
+    const dailyDevotionCron = new Cron(stack, "dailyDevoCron", {
+      schedule: "cron(0 12 * * ? *)",
+      job: {
+        function: {
+          handler: "packages/functions/src/daily-devo.handler",
+          bind: [devotionImageBucket],
+          permissions: [devotionImageBucket, invokeBedrockPolicy],
+          copyFiles: [
+            {
+              from: "firebase-service-account.json",
+              to: "firebase-service-account.json",
+            },
+          ],
+          environment: {
+            DEVOTION_IMAGE_BUCKET: devotionImageBucket.bucketName,
+            DATABASE_READWRITE_URL: dbReadWriteUrl,
+            DATABASE_READONLY_URL: dbReadOnlyUrl,
+            VECTOR_DB_READWRITE_URL: vectorDbReadWriteUrl,
+            VECTOR_DB_READONLY_URL: vectorDbReadOnlyUrl,
+            ...STATIC_ENV_VARS,
           },
-        ],
-        environment: {
-          DEVOTION_IMAGE_BUCKET: devotionImageBucket.bucketName,
-          DATABASE_READWRITE_URL: dbReadWriteUrl,
-          DATABASE_READONLY_URL: dbReadOnlyUrl,
-          VECTOR_DB_READWRITE_URL: vectorDbReadWriteUrl,
-          VECTOR_DB_READONLY_URL: vectorDbReadOnlyUrl,
-          ...STATIC_ENV_VARS,
+          timeout: "5 minutes",
+          memorySize: "512 MB",
         },
-        timeout: "5 minutes",
-        memorySize: "512 MB",
       },
-    },
-  });
+    });
 
-  return {
-    dailyDevotionCron,
-  };
+    return {
+      dailyDevotionCron,
+    };
+  } else {
+    return {};
+  }
 }
