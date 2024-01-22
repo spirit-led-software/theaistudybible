6f5bbe121c1b6485827e445917246d7f54550446
Ian Pascoe <122142608+passgoco@users.noreply.github.com>
2024-01-22 07:08:54 -0500

    Update chat.ts

--- packages/functions/src/chat.ts
+++ packages/functions/src/chat.ts
@@ -189,7 +189,7 @@ async function lambdaHandler(
     const incrementQueryCountPromise = incrementUserQueryCount(userWithRoles.id);
     pendingPromises.push(incrementQueryCountPromise);
 
-    if (modelId && modelId !== 'anthropic.claude-instant-v1' && !hasPlusSync(userWithRoles)) {
+    if (modelId && modelId !== 'anthropic.claude-instant-v1' && !hasPlusSync(userWithRoles) && !isAdminSync(userWithRoles)) {
       return {
         statusCode: 403,
         headers: {
