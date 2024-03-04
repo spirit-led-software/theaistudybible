7411a3244cc2a9b28a5e02fc69720d80e294d80b
Ian Pascoe <ianpascoe@Ians-Mac-3.local>
2024-03-04 12:40:47 -0500

    Consistency of xml building

--- packages/server/src/lib/chat/chat.ts
+++ packages/server/src/lib/chat/chat.ts
@@ -1,12 +1,13 @@
 import { StringOutputParser } from '@langchain/core/output_parsers';
 import type { Chat } from '@revelationsai/core/model/chat';
-import type { Message } from 'ai';
+import type { RAIChatMessage } from '@revelationsai/core/model/chat/message';
+import { XMLBuilder } from 'fast-xml-parser';
 import { PromptTemplate } from 'langchain/prompts';
 import { updateChat } from '../../services/chat';
 import { getLanguageModel } from '../llm';
 import { CHAT_RENAME_CHAIN_PROMPT_TEMPLATE } from './prompts';
 
-export async function aiRenameChat(chat: Chat, history: Message[]) {
+export async function aiRenameChat(chat: Chat, history: RAIChatMessage[]) {
   if (chat.customName) {
     throw new Error('Chat has already been named by the user');
   }
@@ -19,9 +20,8 @@ export async function aiRenameChat(chat: Chat, history: Message[]) {
     .invoke({
       history: history
         .slice(-21)
-        .map(
-          (message) =>
-            `<message>\n<sender>${message.role}</sender>\n<text>${message.content}</text>\n</message>`
+        .map((message) =>
+          new XMLBuilder().build({ message: { sender: message.role, text: message.content } })
         )
         .join('\n')
     })
