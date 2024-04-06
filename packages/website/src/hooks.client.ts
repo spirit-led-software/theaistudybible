a2e18223e8bf1884346ee207825a95ed9b4fa3cd
Ian Pascoe <ian.g.pascoe@gmail.com>
2024-04-06 07:05:15 -0400

    Revert "Add Sentry integration to website package"
    This reverts commit 589d04938aae52bb4f4a47f57c9c0491801d477b.

--- packages/website/src/hooks.client.ts
+++ packages/website/src/hooks.client.ts
@@ -1,24 +1,13 @@
-import * as Sentry from '@sentry/sveltekit';
 import { PUBLIC_API_URL } from '$env/static/public';
 import apiConfig from '@revelationsai/client/configs/api';
 import type { HandleClientError } from '@sveltejs/kit';
 
-// If you don't want to use Session Replay, remove the `Replay` integration, 
-// `replaysSessionSampleRate` and `replaysOnErrorSampleRate` options.
-Sentry.init({
-    dsn: "https://4e3a10962cce1eb46a534d5720440f95@o4506418175737856.ingest.us.sentry.io/4506418505187328",
-    tracesSampleRate: 1,
-    replaysSessionSampleRate: 0.1,
-    replaysOnErrorSampleRate: 1,
-    integrations: [Sentry.replayIntegration()]
-})
-
 apiConfig.url = PUBLIC_API_URL;
 
-export const handleError: HandleClientError = Sentry.handleErrorWithSentry(async ({ error, message }) => {
+export const handleError: HandleClientError = async ({ error, message }) => {
   console.debug(`Error: ${message}`, error);
 
   return {
     message: 'Oops! Something went wrong.'
   };
-});
+};
