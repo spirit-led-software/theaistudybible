009d6bed3d70adc07463bbbd1e019351efa12cf9
Ian Pascoe <ian.g.pascoe@gmail.com>
2024-04-06 07:05:19 -0400

    Revert "Add Sentry integration to auth-ui package"
    This reverts commit a5ca9a92a8f7d808e406d5c7026d5bc4c42bd499.

--- packages/auth-ui/src/hooks.server.ts
+++ packages/auth-ui/src/hooks.server.ts
@@ -1,19 +1,12 @@
-import {sequence} from '@sveltejs/kit/hooks';
-import * as Sentry from '@sentry/sveltekit';
 import { PUBLIC_API_URL, PUBLIC_WEBSITE_URL } from '$env/static/public';
 import apiConfig from '@revelationsai/client/configs/api';
 import { commonCookies } from '@revelationsai/client/utils/cookies';
 import { validNonApiHandlerSession } from '@revelationsai/server/services/session';
 import type { Handle, HandleServerError } from '@sveltejs/kit';
 
-Sentry.init({
-    dsn: "https://4e3a10962cce1eb46a534d5720440f95@o4506418175737856.ingest.us.sentry.io/4506418505187328",
-    tracesSampleRate: 1
-})
-
 apiConfig.url = PUBLIC_API_URL;
 
-export const handle: Handle = sequence(Sentry.sentryHandle(), async ({ resolve, event }) => {
+export const handle: Handle = async ({ resolve, event }) => {
   try {
     const session = event.cookies.get(commonCookies.session);
     if (!session) {
@@ -48,12 +41,12 @@ export const handle: Handle = sequence(Sentry.sentryHandle(), async ({ resolve, 
   }
 
   return resolve(event);
-});
+};
 
-export const handleError: HandleServerError = Sentry.handleErrorWithSentry(({ error, message }) => {
+export const handleError: HandleServerError = ({ error, message }) => {
   console.debug(`Error: ${message}`, error);
 
   return {
     message: 'Oops! Something went wrong.'
   };
-});
+};
--- packages/auth-ui/vite.config.ts
+++ packages/auth-ui/vite.config.ts
@@ -1,14 +1,8 @@
-import { sentrySvelteKit } from "@sentry/sveltekit";
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig, searchForWorkspaceRoot } from 'vite';
 
 export default defineConfig({
-  plugins: [sentrySvelteKit({
-    sourceMapsUploadOptions: {
-      org: "revelationsai",
-      project: "svelte-kit"
-    }
-  }), sveltekit()],
+  plugins: [sveltekit()],
   server: {
     fs: {
       allow: [searchForWorkspaceRoot(process.cwd())]
