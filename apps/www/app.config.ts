487e6279f4dead24a95c62e922b26c05b9b75a85
Ian Pascoe <ian.g.pascoe@gmail.com>
2024-09-12 06:42:46 -0400

    chore: remove solid devtools

--- apps/www/app.config.ts
+++ apps/www/app.config.ts
@@ -1,5 +1,4 @@
 import { defineConfig } from '@solidjs/start/config';
-import devtools from 'solid-devtools/vite';
 import { VitePWA } from 'vite-plugin-pwa';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
@@ -53,9 +52,6 @@ export default defineConfig({
             },
           ],
         },
-      }),
-      devtools({
-        autoname: true,
       }),
     ],
   },
