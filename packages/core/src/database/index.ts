0a24aace61134f7a2a218c0b276c47f24b3800c3
Ian Pascoe <122142608+ian-pascoe@users.noreply.github.com>
2024-09-14 11:01:09 -0400

    Update index.ts

--- packages/core/src/database/index.ts
+++ packages/core/src/database/index.ts
@@ -1,5 +1,5 @@
 import * as schema from '@/core/database/schema';
-import { createClient } from '@libsql/client';
+import { createClient } from '@libsql/client/web';
 import { drizzle } from 'drizzle-orm/libsql';
 import { Resource } from 'sst';
 
