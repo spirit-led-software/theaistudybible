5ef65b01e4c12322e3d17653c6c88d80ba11e189
Ian Pascoe <122142608+ian-pascoe@users.noreply.github.com>
2024-09-14 11:20:17 -0400

    Update middleware.ts

--- apps/www/src/middleware.ts
+++ apps/www/src/middleware.ts
@@ -8,40 +8,5 @@ export default createMiddleware({
       publishableKey: Resource.ClerkPublishableKey.value,
       secretKey: Resource.ClerkSecretKey.value,
     }),
-    ({
-      request: { url },
-      locals: {
-        auth: { sessionClaims, userId },
-      },
-    }) => {
-      const path = new URL(url).pathname;
-      if (
-        path.startsWith('/admin') &&
-        !(sessionClaims?.metadata.roles?.includes('admin') ?? false)
-      ) {
-        return new Response(null, {
-          status: 302,
-          headers: {
-            location: '/',
-          },
-        });
-      }
-      if (path === '/' && userId) {
-        return new Response(null, {
-          status: 302,
-          headers: {
-            location: '/bible',
-          },
-        });
-      }
-      if (path === '/credits' && !userId) {
-        return new Response(null, {
-          status: 302,
-          headers: {
-            location: '/',
-          },
-        });
-      }
-    },
   ],
 });
