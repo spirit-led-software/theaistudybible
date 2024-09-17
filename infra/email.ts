5f92aae9a5f8a68a3156644e4d380fe7cb0c6225
Ian Pascoe <ian.g.pascoe@gmail.com>
2024-09-17 15:43:30 -0400

    fix: email issue

--- infra/email.ts
+++ infra/email.ts
@@ -1,12 +1,9 @@
-import { BASE_DOMAIN, DOMAIN } from './constants';
+import { DOMAIN } from './constants';
 
-export const email =
-  $app.stage === 'production'
-    ? sst.aws.Email.get('Email', BASE_DOMAIN)
-    : new sst.aws.Email('Email', {
-        sender: DOMAIN.properties.value,
-        dns: sst.cloudflare.dns(),
-      });
+export const email = new sst.aws.Email('Email', {
+  sender: DOMAIN.properties.value,
+  dns: sst.cloudflare.dns(),
+});
 
 export const emailQueue = new sst.aws.Queue('EmailQueue', {
   fifo: true,
