37745c83635bef30d02669c863e57bc41d1c6931
Ian Pascoe <ian.g.pascoe@gmail.com>
2024-09-18 15:47:57 -0400

    fix: profiling

--- apps/www/sentry.server.mjs
+++ apps/www/sentry.server.mjs
@@ -3,7 +3,8 @@ import * as Sentry from '@sentry/solidstart';
 
 Sentry.init({
   dsn: 'https://d05be7f6d9044060ebd25396600d21cf@o4507103632359424.ingest.us.sentry.io/4507974022266880',
-  sampleRate: process.env.PUBLIC_STAGE === 'production' ? 1.0 : 0.1,
+  tracesSampleRate: process.env.PUBLIC_STAGE === 'production' ? 1.0 : 0.1,
+  profilesSampleRate: process.env.PUBLIC_STAGE === 'production' ? 1.0 : 0.1,
   environment: process.env.PUBLIC_STAGE,
   debug: process.env.PUBLIC_STAGE !== 'production',
   registerEsmLoaderHooks: {
--- apps/www/src/entry-client.tsx
+++ apps/www/src/entry-client.tsx
@@ -5,9 +5,15 @@ import { StartClient, mount } from '@solidjs/start/client';
 
 Sentry.init({
   dsn: 'https://d05be7f6d9044060ebd25396600d21cf@o4507103632359424.ingest.us.sentry.io/4507974022266880',
-  integrations: [solidRouterBrowserTracingIntegration(), Sentry.replayIntegration()],
+  integrations: [
+    solidRouterBrowserTracingIntegration(),
+    Sentry.browserProfilingIntegration(),
+    Sentry.replayIntegration(),
+  ],
   environment: import.meta.env.PUBLIC_STAGE,
-  sampleRate: import.meta.env.PUBLIC_STAGE === 'production' ? 1.0 : 0.1,
+  tracesSampleRate: import.meta.env.PUBLIC_STAGE === 'production' ? 1.0 : 0.1,
+  profilesSampleRate: import.meta.env.PUBLIC_STAGE === 'production' ? 1.0 : 0.1,
+  replaysOnErrorSampleRate: import.meta.env.PUBLIC_STAGE === 'production' ? 0.5 : 0,
   debug: import.meta.env.PUBLIC_STAGE !== 'production',
 });
 
