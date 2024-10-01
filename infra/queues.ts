85665c37aec4a6a60203ce7cb38c69cf07a81e1c
Ian Pascoe <ian.g.pascoe@gmail.com>
2024-10-01 16:19:58 -0400

    fix: install jsdom

--- infra/queues.ts
+++ infra/queues.ts
@@ -1,35 +1,24 @@
 export const indexBibleQueue = new sst.aws.Queue('IndexBibleQueue');
-indexBibleQueue.subscribe(
-  {
-    handler: 'apps/functions/src/queues/subscribers/bibles/index-bible.handler',
+indexBibleQueue.subscribe({
+  handler: 'apps/functions/src/queues/subscribers/bibles/index-bible.handler',
+  nodejs: {
+    install: ['jsdom'],
   },
-  {
-    batch: {
-      size: 1,
-    },
-  },
-);
+  timeout: '15 minutes',
+});
 
 export const indexBibleChapterQueue = new sst.aws.Queue('IndexBibleChapterQueue');
-indexBibleChapterQueue.subscribe(
-  {
-    handler: 'apps/functions/src/queues/subscribers/bibles/index-chapter/index.handler',
-  },
-  {
-    batch: {
-      size: 1,
-    },
-  },
-);
+indexBibleChapterQueue.subscribe({
+  handler: 'apps/functions/src/queues/subscribers/bibles/index-chapter/index.handler',
+  timeout: '15 minutes',
+});
 
 export const profileImagesQueue = new sst.aws.Queue('ProfileImagesQueue');
 profileImagesQueue.subscribe({
   handler: 'apps/functions/src/queues/subscribers/profile-images.handler',
 });
 
-export const emailQueue = new sst.aws.Queue('EmailQueue', {
-  fifo: true,
-});
+export const emailQueue = new sst.aws.Queue('EmailQueue');
 emailQueue.subscribe({
   handler: 'apps/functions/src/queues/subscribers/email/index.handler',
 });
