26040dc12ad8fa601bf9933da2507ba777e311b7
Ian Pascoe <ian.g.pascoe@gmail.com>
2024-10-27 11:04:43 -0400

    fix: memoize user info

--- apps/www/src/components/auth/user-avatar.tsx
+++ apps/www/src/components/auth/user-avatar.tsx
@@ -1,20 +1,26 @@
 import { useAuth } from '@/www/contexts/auth';
 import type { PolymorphicProps } from '@kobalte/core';
 import type { ImageRootProps } from '@kobalte/core/image';
-import type { ValidComponent } from 'solid-js';
+import { type ValidComponent, createMemo } from 'solid-js';
 import { Avatar, AvatarFallback, AvatarImage } from '../ui/avatar';
 
 export const UserAvatar = <T extends ValidComponent>(
   props: PolymorphicProps<T, ImageRootProps>,
 ) => {
   const { user } = useAuth();
+
+  const src = createMemo(() => user()?.image || undefined);
+  const fallback = createMemo(
+    () =>
+      `${user()?.firstName?.charAt(0) || user()?.email?.charAt(0) || '?'}${
+        user()?.lastName?.charAt(0) || ''
+      }`,
+  );
+
   return (
     <Avatar {...(props as ImageRootProps)}>
-      <AvatarImage src={user()?.image || undefined} />
-      <AvatarFallback>
-        {user()?.firstName?.charAt(0) || user()?.email?.charAt(0) || '?'}
-        {user()?.lastName?.charAt(0) || ''}
-      </AvatarFallback>
+      <AvatarImage src={src()} />
+      <AvatarFallback>{fallback()}</AvatarFallback>
     </Avatar>
   );
 };
