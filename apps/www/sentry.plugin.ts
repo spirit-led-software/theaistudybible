07857638315fc8923e881af30427c2ece860cb43
Ian Pascoe <122142608+ian-pascoe@users.noreply.github.com>
2024-10-31 07:03:03 -0400

    Update sentry.plugin.ts

--- apps/www/sentry.plugin.ts
+++ apps/www/sentry.plugin.ts
@@ -1,7 +1,10 @@
 import * as Sentry from '@sentry/solidstart';
 
+const isProduction = process.env.PUBLIC_STAGE === 'production';
+const isDev = process.env.DEV;
+
 Sentry.init({
   dsn: process.env.PUBLIC_SENTRY_DSN,
-  tracesSampleRate: process.env.PUBLIC_STAGE === 'production' ? 1.0 : 0.1,
+  tracesSampleRate: isDev ? 0 : isProd ? 1.0 : 0.5,
   environment: process.env.PUBLIC_STAGE,
 });
