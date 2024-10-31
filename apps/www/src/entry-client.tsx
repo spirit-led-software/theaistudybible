239a42327d0659538fd7bfe766cf253bc2b9d4f4
Ian Pascoe <ian.g.pascoe@gmail.com>
2024-10-30 20:56:41 -0400

    fix: sentry sample rates on different environments

--- apps/www/src/entry-client.tsx
+++ apps/www/src/entry-client.tsx
@@ -3,6 +3,9 @@ import * as Sentry from '@sentry/solidstart';
 import { solidRouterBrowserTracingIntegration } from '@sentry/solidstart/solidrouter';
 import { StartClient, mount } from '@solidjs/start/client';
 
+const isProduction = import.meta.env.PUBLIC_STAGE === 'production';
+const isDev = import.meta.env.DEV;
+
 Sentry.init({
   dsn: import.meta.env.PUBLIC_SENTRY_DSN,
   integrations: [
@@ -10,9 +13,9 @@ Sentry.init({
     Sentry.replayIntegration(),
     Sentry.browserProfilingIntegration(),
   ],
-  tracesSampleRate: import.meta.env.PUBLIC_STAGE === 'production' ? 1.0 : 0,
-  replaysOnErrorSampleRate: import.meta.env.PUBLIC_STAGE === 'production' ? 0.25 : 0,
-  profilesSampleRate: import.meta.env.PUBLIC_STAGE === 'production' ? 1.0 : 0,
+  tracesSampleRate: isDev ? 0 : isProduction ? 1.0 : 0.5,
+  replaysOnErrorSampleRate: isProduction ? 0.25 : 0,
+  profilesSampleRate: isDev ? 0 : isProduction ? 0.75 : 0.5,
   environment: import.meta.env.PUBLIC_STAGE,
 });
 
