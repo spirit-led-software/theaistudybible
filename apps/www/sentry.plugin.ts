cceb4b2178f83334b54f8ba14492619eb2188463
Ian Pascoe <122142608+ian-pascoe@users.noreply.github.com>
2024-10-31 06:56:49 -0400

    Update sentry.plugin.ts

--- apps/www/sentry.plugin.ts
+++ apps/www/sentry.plugin.ts
@@ -1,26 +1,7 @@
-import * as Sentry from '@sentry/bun';
-import { defineNitroPlugin } from 'nitropack/runtime/plugin';
-import { H3Error } from 'vinxi/http';
+import * as Sentry from '@sentry/solidstart';
 
-export default defineNitroPlugin((nitroApp) => {
-  Sentry.init({
-    dsn: process.env.PUBLIC_SENTRY_DSN,
-    tracesSampleRate: process.env.PUBLIC_STAGE === 'production' ? 1.0 : 0.1,
-    environment: process.env.PUBLIC_STAGE,
-  });
-
-  nitroApp.hooks.hook('error', (error) => {
-    if (error instanceof H3Error) {
-      if (error.statusCode >= 500 && error.statusCode < 600) {
-        Sentry.captureException(error);
-      }
-      return;
-    }
-    Sentry.captureException(error);
-  });
-
-  nitroApp.hooks.hookOnce('close', async () => {
-    await Sentry.flush();
-    await Sentry.close();
-  });
+Sentry.init({
+  dsn: process.env.PUBLIC_SENTRY_DSN,
+  tracesSampleRate: process.env.PUBLIC_STAGE === 'production' ? 1.0 : 0.1,
+  environment: process.env.PUBLIC_STAGE,
 });
