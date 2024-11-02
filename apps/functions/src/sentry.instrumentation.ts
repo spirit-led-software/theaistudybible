04a2571265a06cc879a833b603e942399d0eeb75
Ian Pascoe <122142608+ian-pascoe@users.noreply.github.com>
2024-11-02 07:50:39 -0400

    Update sentry.instrumentation.ts

--- apps/functions/src/sentry.instrumentation.ts
+++ apps/functions/src/sentry.instrumentation.ts
@@ -1,5 +1,4 @@
 import * as Sentry from '@sentry/aws-serverless';
-import { nodeProfilingIntegration } from '@sentry/profiling-node';
 import { Resource } from 'sst';
 
 const isProd = Resource.Stage.value === 'production';
@@ -7,7 +6,6 @@ const isDev = Resource.Dev.value === 'true';
 
 Sentry.init({
   dsn: 'https://795730c6439d3fbbb5578249c85386b0@o4508042228006912.ingest.us.sentry.io/4508135125876736',
-  integrations: [nodeProfilingIntegration()],
   tracesSampleRate: isDev ? 0 : isProd ? 1.0 : 0.5,
-  profilesSampleRate: isDev ? 0 : isProd ? 0.75 : 0.25,
+  environment: Resource.stage.value,
 });
