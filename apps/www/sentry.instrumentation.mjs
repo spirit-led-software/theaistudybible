a97d77542139c518d79cad1e48762579e29a1a41
Ian Pascoe <122142608+ian-pascoe@users.noreply.github.com>
2024-11-03 20:35:17 -0500

    Update sentry.instrumentation.mjs

--- apps/www/sentry.instrumentation.mjs
+++ apps/www/sentry.instrumentation.mjs
@@ -1,10 +1,26 @@
-import * as Sentry from '@sentry/bun';
+// TODO: issue with standard instrumentation:
+// https://github.com/getsentry/sentry-javascript/issues/12891
+import {
+  BunClient,
+  defaultStackParser,
+  getDefaultIntegrations,
+  makeFetchTransport,
+  setCurrentClient
+} from '@sentry/bun'
 
 const isProd = process.env.PUBLIC_STAGE === 'production';
 const isDev = process.env.DEV;
 
-Sentry.init({
+const integrations = getDefaultIntegrations({}).filter((integration) => integration.name !== 'Http')
+
+const client = new BunClient({
   dsn: process.env.PUBLIC_SENTRY_DSN,
+  stackParser: defaultStackParser,
+  transport: makeFetchTransport,
   tracesSampleRate: isDev ? 0 : isProd ? 1.0 : 0.5,
+  integrations,
   environment: process.env.PUBLIC_STAGE,
-});
+})
+
+setCurrentClient(client)
+client.init()
