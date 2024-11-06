4665ab9d829ce7406b0eb5b48d026f5fdfe477a0
Ian Pascoe <ian.g.pascoe@gmail.com>
2024-11-06 14:37:24 -0500

    fix: analytics not working

--- apps/workers/src/proxy/analytics.ts
+++ apps/workers/src/proxy/analytics.ts
@@ -3,20 +3,35 @@ type Env = {
   ASSET_HOST: string;
 };
 
+async function handleRequest(request: Request, env: Env, ctx: ExecutionContext) {
+  const url = new URL(request.url);
+  const pathname = url.pathname;
+  const search = url.search;
+  const pathWithParams = pathname + search;
+
+  if (pathname.startsWith('/static/')) {
+    return await retrieveStatic(request, env, ctx, pathWithParams);
+  }
+  return await forwardRequest(request, env, pathWithParams);
+}
+
+async function retrieveStatic(request: Request, env: Env, ctx: ExecutionContext, pathname: string) {
+  let response = await caches.default.match(request);
+  if (!response) {
+    response = await fetch(`${env.ASSET_HOST}${pathname}`);
+    ctx.waitUntil(caches.default.put(request, response.clone()));
+  }
+  return response;
+}
+
+async function forwardRequest(request: Request, env: Env, pathWithSearch: string) {
+  const originRequest = new Request(request);
+  originRequest.headers.delete('cookie');
+  return await fetch(`${env.API_HOST}${pathWithSearch}`, originRequest);
+}
+
 export default {
   async fetch(request, env, ctx) {
-    const url = new URL(request.url);
-    const pathname = url.pathname;
-    const pathWithParams = `${pathname}${url.search}`;
-    if (pathname.startsWith('/static/')) {
-      const defaultCache = caches.default as Cache;
-      let response = await defaultCache.match(request);
-      if (!response) {
-        response = await fetch(`${env.ASSET_HOST}${pathWithParams}`);
-        ctx.waitUntil(defaultCache.put(request, response.clone()));
-      }
-      return response;
-    }
-    return await fetch(`${env.API_HOST}${pathWithParams}`, request);
+    return await handleRequest(request, env, ctx);
   },
 } satisfies ExportedHandler<Env>;
