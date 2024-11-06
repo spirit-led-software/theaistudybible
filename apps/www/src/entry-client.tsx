53efbc26ce464d1c724e7d255f8190822d84eea7
Ian Pascoe <122142608+ian-pascoe@users.noreply.github.com>
2024-11-06 14:55:23 -0500

    Update entry-client.tsx

--- apps/www/src/entry-client.tsx
+++ apps/www/src/entry-client.tsx
@@ -5,7 +5,7 @@ import { StartClient, mount } from '@solidjs/start/client';
 import posthog from 'posthog-js';
 
 const isProd = import.meta.env.PUBLIC_STAGE === 'production';
-const isDev = import.meta.dev;
+const isDev = import.meta.env.PUBLIC_DEV === 'true';
 
 posthog.init(import.meta.env.PUBLIC_POSTHOG_API_KEY, {
   api_host: import.meta.env.PUBLIC_POSTHOG_API_HOST,
