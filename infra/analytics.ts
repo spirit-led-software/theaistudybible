7c404daf8064c6a330fa777d5402cea8a1813007
Ian Pascoe <ian.g.pascoe@gmail.com>
2024-11-14 09:45:51 -0500

    fix: conflict with dns record

--- infra/analytics.ts
+++ infra/analytics.ts
@@ -1,6 +1,6 @@
 import { DOMAIN, POSTHOG_API_HOST, POSTHOG_ASSETS_HOST } from './constants';
 
-const ANALYTICS_DOMAIN = $interpolate`piggy.${DOMAIN.value}`;
+const ANALYTICS_DOMAIN = $interpolate`hog.${DOMAIN.value}`;
 
 export const analyticsApi = new sst.aws.Router('AnalyticsApi', {
   routes: { '/*': POSTHOG_API_HOST.value, '/static/*': POSTHOG_ASSETS_HOST.value },
