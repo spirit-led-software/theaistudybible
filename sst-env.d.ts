40bc87adaf33c425ea705edf05de973eeae344fe
Ian Pascoe <ian.g.pascoe@gmail.com>
2025-02-26 16:53:16 -0500

    Add SST environment type definitions for new services and routers
    - Extend sst-env.d.ts with new type definitions
    - Add AnalyticsApiRouter type
    - Include WebAppCdnLoggingBucket definition
    - Add regional WebAppServerServiceRouter types for eu-west-1 and us-east-1

--- sst-env.d.ts
+++ sst-env.d.ts
@@ -17,6 +17,10 @@ declare module "sst" {
       "type": "sst.sst.Linkable"
       "url": string
     }
+    "AnalyticsApiRouter": {
+      "type": "sst.aws.Router"
+      "url": string
+    }
     "AnthropicApiKey": {
       "type": "sst.sst.Secret"
       "value": string
@@ -252,6 +256,18 @@ declare module "sst" {
     "VoyageAiApiKey": {
       "type": "sst.sst.Secret"
       "value": string
+    }
+    "WebAppCdnLoggingBucket": {
+      "name": string
+      "type": "sst.aws.Bucket"
+    }
+    "WebAppServerServiceRouter-eu-west-1": {
+      "type": "sst.aws.Router"
+      "url": string
+    }
+    "WebAppServerServiceRouter-us-east-1": {
+      "type": "sst.aws.Router"
+      "url": string
     }
     "WebAppUrl": {
       "type": "asb.asb.Constant"
